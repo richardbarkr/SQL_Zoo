a637b5f8188d46ab11d7dfdd94fc1b591744a205
Richard Barker <74937909+richardbarkr@users.noreply.github.com>
2020-12-14 17:38:46 +0000

    Update 9- Window Function.sql

--- 9- Window Function.sql
+++ 9- Window Function.sql
@@ -42,7 +42,7 @@ SELECT constituency, party, votes,
 
 SELECT t.constituency, t.party
      FROM
-          (SELECT g.constituency, g.party, RANK() OVER (PARTITION BY g.constituency ORDER BY g.votes DESC) as     posn
+          (SELECT g.constituency, g.party, RANK() OVER (PARTITION BY g.constituency ORDER BY g.votes DESC) as posn
      FROM ge g
           WHERE g.constituency BETWEEN 'S14000021' AND 'S14000026'
           AND g.yr  = 2017) as t
